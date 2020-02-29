ab48535390ee1a70364dc3be1f2a5ce6d5681a7a
Pokour <pokour@gmail.com>
2020-02-29 11:22:59 +0530

    test case added

--- tutorials/encode.js
+++ tutorials/encode.js
@@ -4,16 +4,18 @@ function generate(a,b){
     if(a.length < b.length){
         for(i=0; i<a.length; i++){
             encode +=  a[i]+b[i];
+            console.log('LOOP', i);
         }
         encode += b.substring(a.length,b.length);        
     }
     else{
         for(i=0; i<b.length; i++){
             encode += a[i] + b[i];
+            console.log('LOOP', i);
         }
         encode += a.substring(b.length,a.length);        
     }
     return encode;
 }
-console.log( generate('1234','abcdef') );
+console.log( generate('1234','abcdefghij') );
 
