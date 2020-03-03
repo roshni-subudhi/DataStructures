03222d6ed2d89bc11c454fe13d28378160260a09
roshni-subudhi <38189190+roshni-subudhi@users.noreply.github.com>
2020-03-03 16:03:10 +0530

    IIF created

--- tutorials/iife.js
+++ tutorials/iife.js
@@ -1,5 +1,13 @@
+(function (){
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
 
 
+(function (goodLuck){
+    var score = Math.random() * 10 - goodLuck;
+    console.log(score >= 5);
+})(5);
 
 
 
